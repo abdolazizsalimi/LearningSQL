63d7341a53caf69c81e6a3dd3701ca5a19a11391
abdolazizsalimi <aziz.s6882@gmail.com>
2023-05-17 21:54:54 +0330

    add :[05-create employees table ]

---
+++ codeTemp/05-creat-employees-table.sql
@@ -0,0 +1,8 @@
+
+CREATE TABLE IF NOT EXISTS employees(
+    company_name VARCHAR(64),
+    company_address VARCHAR(255),
+    yearly_revenue FLOAT(9,3), -- Allowed numbers :123456.789 , 111111.222 , 121.4444
+    -- yearly_revenue DECIMAL,
+    is_hiring BOOLEAN 
+);
