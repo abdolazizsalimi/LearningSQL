fdecf52240896f366d8698c743c1aa6ffe62cbc2
abdolazizsalimi <aziz.s6882@gmail.com>
2023-05-19 00:25:18 +0330

    add-needfix :[06 and 07 neeed sleeep ]

---
+++ codeTemp/06-create-conversions-table.sql
@@ -0,0 +1,7 @@
+USE talently;
+CREATE TABLE IF NOT EXISTS conversation(
+    user_name VARCHAR(255),
+    employee_name VARCHAR(255),
+    massage TEXT,
+    date_sent TIMESTAMP
+);
---
+++ codeTemp/07-insert-values-into-tables.sql
@@ -0,0 +1,16 @@
+INSERT INTO employees 
+(company_name , company_address , yearly_revenue , is_hiring) 
+VALUES('');
+
+
+
+-- INSERT INTO employees 
+-- (company_name , company_address , yearly_revenue , is_hiring) 
+-- VALUES('');
+-- INSERT INTO employees 
+-- (company_name , company_address , yearly_revenue , is_hiring) 
+-- VALUES('');
+-- INSERT INTO employees 
+-- (company_name , company_address , yearly_revenue , is_hiring) 
+-- VALUES('');
+
