10d27c009d1f47a64ee35029a8c935afcc2d7e28
abdolazizsalimi <aziz.s6882@gmail.com>
2023-05-21 19:14:43 +0330

    add :[insert violating data ]

---
+++ codeTemp/16-insert-violating-data.sql
@@ -0,0 +1,14 @@
+USE talently;
+-- did not accept negative year salary : we have add check 
+INSERT INTO users (full_name , year_salary , current_status) VALUES (
+    'ALi Salimi',
+    -100,
+    'employed'
+); 
+
+
+-- INSERT INTO users (full_name , year_salary , current_status) VALUES (
+--     'ALi Salimi',
+--     100,
+--     'employed'
+-- ); 
